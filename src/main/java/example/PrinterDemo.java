7c72303298e4d6f55d1eed01c35e26fcbf759967
kremlianski <kremlianski@gmail.com>
2019-12-24 19:00:56 +0300

    make methods generic

--- src/main/java/example/PrinterDemo.java
+++ src/main/java/example/PrinterDemo.java
@@ -2,8 +2,27 @@ package example;
 
 import static ru.exxo.jutil.Printer.*;
 public class PrinterDemo {
+    private static class Demo {
+        public Demo(String str) {
+            this.str = str;
+        }
+
+        private String str;
+
+        @Override
+        public String toString() {
+            return "Demo{" +
+                    "str='" + str + '\'' +
+                    '}';
+        }
+    }
     public static void main(String[] args) {
+
+        Demo demo = new Demo("It's a Demo");
         println("I can print!");
+        println(10);
+        println(true);
+        println(demo);
     }
 }
 
--- src/main/java/ru/exxo/jutil/Printer.java
+++ src/main/java/ru/exxo/jutil/Printer.java
@@ -1,10 +1,12 @@
 package ru.exxo.jutil;
 
 public interface Printer {
-    static void println(String str) {
-        System.out.println(str);
+
+    static <T> void print(T t) {
+        System.out.print(t);
     }
-    static void print(String str) {
-        System.out.print(str);
+
+    static <T> void println(T t) {
+        System.out.println(t);
     }
 }
