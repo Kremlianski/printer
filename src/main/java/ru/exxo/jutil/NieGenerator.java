c5fc1aea6cb71265fcfdd2976cad96efbebb325d
kremlianski <kremlianski@gmail.com>
2021-04-30 23:17:00 +0300

    add javadoc

--- src/main/java/ru/exxo/jutil/NieGenerator.java
+++ src/main/java/ru/exxo/jutil/NieGenerator.java
@@ -1,3 +1,6 @@
+/*  for generating custom runtime exception
+ *  an exception class must have a constructor with a String argument
+ */
 package ru.exxo.jutil;
 
 import java.lang.reflect.InvocationTargetException;
@@ -11,26 +14,57 @@ public class NieGenerator<E extends RuntimeException> {
     private final String pattern;
     private final Class<?> clazz;
 
+    /**
+     *
+     * @param exceptionClass a class of generated custom exception
+     * @param pattern a pattern of exception message instead of default value
+     *                "Method '%s' of class '%s' has not implemented yet"
+     * @param clazz a class that possesses the method which should throw an exception
+     */
     public NieGenerator(Class<E> exceptionClass, String pattern, Class<?> clazz) {
         this.exceptionClass = exceptionClass;
         this.pattern = pattern;
         this.clazz = clazz;
     }
-
+    /**
+     *
+     * @param exceptionClass a class of generated custom exception
+     * @param clazz a class that possesses the method which should throw an exception
+     */
     public NieGenerator(Class<E> exceptionClass, Class<?> clazz) {
         this.exceptionClass = exceptionClass;
         this.pattern = messagePattern;
         this.clazz = clazz;
     }
-
+    /**
+     * generates a custom exception with predefined in generator instance parameters
+     *
+     * @return generated exception
+     */
     public E nie() {
         return nie(clazz, pattern, exceptionClass);
     }
-
+    /**
+     * for non-static methods
+     *
+     * @param context for non-static methods it should be 'this'
+     * @param pattern a pattern of exception message instead of default value
+     *                "Method '%s' of class '%s' has not implemented yet"
+     * @param exceptionClass a class of generated custom exception
+     * @return generated exception
+     */
     public static <E extends RuntimeException> E  nie(Object context, String pattern, Class<E> exceptionClass) {
         return nie(context.getClass(), pattern, exceptionClass);
     }
-
+    /**
+     * for static methods
+     *
+     * @param clazz a class that possesses the method which should throw an exception
+     * @param pattern a pattern of exception message instead of default value
+     *                "Method '%s' of class '%s' has not implemented yet"
+     * @param exceptionClass a class of generated custom exception
+     * @return generated exception
+     */
     public static <E extends RuntimeException> E nie(Class<?> clazz, String pattern, Class<E> exceptionClass) {
         String msg = getMessage(clazz, pattern);
 
@@ -40,14 +74,24 @@ public class NieGenerator<E extends RuntimeException> {
             throw new RuntimeException(String.format("Something went wrong while throwing '%s'",
                     exceptionClass.getCanonicalName()));
         }
-
-
     }
-
+    /**
+     * for non-static methods
+     *
+     * @param context for non-static methods it should be 'this'
+     * @param exceptionClass a class of generated custom exception
+     * @return generated exception
+     */
     public static <E extends RuntimeException> E  nie(Object context, Class<E> exceptionClass) {
         return nie(context, messagePattern, exceptionClass);
     }
-
+    /**
+     * for static methods
+     *
+     * @param clazz a class that possesses the method which should throw an exception
+     * @param exceptionClass a class of generated custom exception
+     * @return generated exception
+     */
     public static <E extends RuntimeException> E  nie (Class<?> clazz, Class<E> exceptionClass) {
         return nie(clazz, messagePattern, exceptionClass);
     }
--- src/main/java/ru/exxo/jutil/UoeGenerator.java
+++ src/main/java/ru/exxo/jutil/UoeGenerator.java
@@ -7,37 +7,78 @@ public class UoeGenerator {
     private final String pattern;
     private final Class<?> clazz;
 
+    /**
+     *
+     * @param pattern a pattern of exception message instead of default value
+     *                "Method '%s' of class '%s' has not implemented yet"
+     * @param contextClass a class that possesses the method which should throw an exception
+     */
     public UoeGenerator(String pattern, Class<?> contextClass) {
         this.pattern = pattern;
         this.clazz = contextClass;
     }
 
+    /**
+     *
+     * @param contextClass a class that possesses the method which should throw an exception
+     */
     public UoeGenerator(Class<?> contextClass) {
         this.clazz = contextClass;
         this.pattern = messagePattern;
     }
 
-
+    /**
+     * generates UnsupportedOperationException with predefined in generator instance parameters
+     *
+     * @return generated exception
+     */
     public UnsupportedOperationException uoe() {
         return uoe(clazz, pattern);
     }
 
+    /**
+     * for non-static methods
+     *
+     * @param context for non-static methods it should be 'this'
+     * @param pattern a pattern of exception message instead of default value
+     *                "Method '%s' of class '%s' has not implemented yet"
+     * @return generated exception
+     */
     public static UnsupportedOperationException uoe(Object context, String pattern) {
         return uoe(context.getClass(), pattern);
     }
 
+    /**
+     * for static methods
+     *
+     * @param clazz a class that possesses the method which should throw an exception
+     * @param pattern a pattern of exception message instead of default value
+     *                "Method '%s' of class '%s' has not implemented yet"
+     * @return generated exception
+     */
     public static UnsupportedOperationException uoe(Class<?> clazz, String pattern) {
-        String className = clazz.getCanonicalName();
 
         String msg = NieGenerator.getMessage(clazz, pattern);
 
         return new UnsupportedOperationException(msg);
     }
 
+    /**
+     * for non-static methods
+     *
+     * @param context for non-static methods it should be 'this'
+     * @return generated exception
+     */
     public static UnsupportedOperationException uoe(Object context) {
         return uoe(context, messagePattern);
     }
 
+    /**
+     * for static methods
+     *
+     * @param clazz class that possesses the method which should throw an exception
+     * @return generated exception
+     */
     public static UnsupportedOperationException uoe (Class<?> clazz) {
         return uoe(clazz, messagePattern);
     }
